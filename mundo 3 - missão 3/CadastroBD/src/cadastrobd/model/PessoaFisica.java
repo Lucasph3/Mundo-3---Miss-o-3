794732ed2721d83965e51efbd4351fee87b413c4
Lucas Oliveira <119544801+Lucasph3@users.noreply.github.com>
2024-05-15 11:24:20 -0400

    Add files via upload

---
+++ mundo 3 - missão 3/CadastroBD/src/cadastrobd/model/PessoaFisica.java
@@ -0,0 +1,31 @@
+package cadastrobd.model;
+
+
+
+public class PessoaFisica extends Pessoa {
+    private String cpf;
+
+    public PessoaFisica() {
+        super();
+        this.cpf = "";
+    }
+
+    public PessoaFisica(int id, String nome, String logradouro, String cidade, String estado, String telefone, String email, String cpf) {
+        super(id, nome, logradouro, cidade, estado, telefone, email);
+        this.cpf = cpf;
+    }
+
+    @Override
+    public void exibir() {
+        super.exibir();
+        System.out.println("CPF: " + this.cpf);
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+}
